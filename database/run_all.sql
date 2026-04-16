fc673d6738e31b3c942136ea56a3a20c8ef8aa45
mnagamallikmail-ui <mnagamallik.mail@gmail.com>
2026-04-16 21:37:28 +0530

    Fix run_all.sql for PostgreSQL compatibility

--- database/run_all.sql
+++ database/run_all.sql
@@ -1,10 +1,10 @@
-alter session set container=FREEPDB1;
-@00_schema_setup.sql
-conn BWVR/bwvr_pass@localhost:1521/FREEPDB1
-@01_sequences.sql
-@02_tables_core.sql
-@03_tables_report.sql
-@04_tables_audit.sql
-@05_views.sql
-@06_tables_auth.sql
-exit
+-- PostgreSQL-compatible run script
+
+\i database/00_schema_setup.sql
+\i database/01_sequences.sql
+\i database/02_tables_core.sql
+\i database/03_tables_report.sql
+\i database/04_tables_audit.sql
+\i database/05_views.sql
+\i database/06_tables_auth.sql
+``
